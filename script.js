f4f6ca2e2605a12fcd9d3d60cdcc462cf5ecb178
unknown <wardle2106@gmail.com>
2018-06-07 15:20:19 -0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+//STICKY NAV
   let navOffset = $('.about-section').offset().top;
 
   $('nav').wrap('<div class= "nav-placeholder"></div>');
@@ -15,4 +16,67 @@ $(document).ready(function(){
     }
   });
 
+
+
+
+  //SMOOTH SCROLLING SNIPPET*******************
+
+
+
+  // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+
+  //ANIMATE NAV LINK ON SCROLL
+
+//NEED TO FIGURE OUT HOW TO DISPLAY, I THINK SPECIFICTY IS THE PROBLEM
+  $(window).scroll(function(){
+    let scrollLink = $('.scroll');
+    let scrollbarLocation = $(this).scrollTop();
+
+    scrollLink.each(function(){
+
+      let sectionOffset = $(this.hash).offset().top;
+
+      if (sectionOffset <= scrollbarLocation) {
+        $(this).parent().addClass('.nav li a::after');
+        $(this).parent().siblings().removeClass('.nav li a::after');
+      }
+    });
+  });
 });
